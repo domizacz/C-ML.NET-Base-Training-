65500fd4e68935d7dfdf78aef47912421e6b8561
Dom Dom <domizacz@gmail.com>
2020-11-16 09:35:13 -0600

    First commit

---
+++ mi/HousingData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML.Data;
+
+namespace mi
+{
+    internal class HousingData
+    {
+        [LoadColumn(0)]
+        public float Longitude { get; set; }
+        [LoadColumn(1)]
+        public float latitude { get; set; }
+        [LoadColumn(2)]
+        public float housingMedianAge { get; set; }
+        [LoadColumn(3)]
+        public float totalRooms { get; set; }
+        [LoadColumn(4)]
+        public float totalBedrooms { get; set; }
+        [LoadColumn(5)]
+        public float population { get; set; }
+        [LoadColumn(6)]
+        public float households { get; set; }
+        [LoadColumn(7)]
+        public float medianIncome { get; set; }
+        [LoadColumn(8), ColumnName("Label")]
+        public float medianHouseValue { get; set; }
+        [LoadColumn(9)]
+        public string oceanproximity { get; set; }
+    }
+}
---
+++ mi/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ML;
+using System;
+using System.Linq;
+
+namespace mi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+            var data = context.Data.LoadFromTextFile<HousingData>("./housing.csv", hasHeader: true, separatorChar: ',');
+            
+            var split = context.Data.TrainTestSplit(data, testFraction: 0.2);
+
+            var features = split.TrainSet.Schema
+                .Select(col => col.Name)
+                .Where(colName => colName != "Label" && colName != "oceanproximity")
+                .ToArray();
+            var pipeline = context.Transforms.Text.FeaturizeText("Text", "oceanproximity")
+                .Append(context.Transforms.Concatenate("Features", features))
+                .Append(context.Transforms.Concatenate("Features", "Features", "Text"))
+                .Append(context.Regression.Trainers.LbfgsPoissonRegression);
+
+            var model = pipeline.Fit(split.TrainSet);
+
+            var predictions = model.Transform(split.TestSet);
+
+            var metrics = context.Regression.Evaluate(predictions);
+
+            Console.WriteLine($"R^2 - {metrics.RSquared}");
+            Console.ReadLine();
+        }
+    }
+}
